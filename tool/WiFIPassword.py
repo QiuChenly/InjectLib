3f47dc7245d8b5a0c1e0810012a08a2338c7ace7
QiuChenly <QiuChenly@outlook.com>
2024-02-22 00:11:42 +0800

    fix macOS 13 以下系统运行0xD崩溃的问题。

--- tool/WiFIPassword.py
+++ tool/WiFIPassword.py
@@ -8,11 +8,12 @@ import json
 import hashlib
 from Crypto.Cipher import AES
 import collections
+from urllib.parse import quote, urlencode
 
 
-def getMd5(str):
+def getMd5(keys: str):
     md5 = hashlib.md5()
-    md5.update(str)
+    md5.update(keys.encode())
     return md5.hexdigest()
 
 
@@ -24,7 +25,7 @@ def openwrtWIFIList(radio="radio0"):
     """
     u = "http://192.168.55.1/ubus"
     d = (
-        '{"jsonrpc":"2.0","id":46,"method":"call","params":["618eaaa669e793ae30e5e7770e324383","iwinfo","scan",{"device":"'
+        '{"jsonrpc":"2.0","id":46,"method":"call","params":["997f27479382c7d47bf2eeecf97f1597","iwinfo","scan",{"device":"'
         + radio
         + '"}]}'
     )
@@ -34,65 +35,73 @@ def openwrtWIFIList(radio="radio0"):
     return res
 
 
+def getWIFI(ssid, bssid):
+    dt = collections.OrderedDict()
+    dt["origChanId"] = "xiaomi"
+    dt["appId"] = "A0008"
+    dt["ts"] = "1459936625905"
+    dt["netModel"] = "w"
+    dt["chanId"] = "guanwang"
+    dt["imei"] = "357541051318147"
+    dt["qid"] = ""
+    dt["mac"] = "e8:92:a4:9b:16:42"
+    dt["capSsid"] = "hijack"
+    dt["lang"] = "cn"
+    dt["longi"] = "103.985752"
+    dt["nbaps"] = ""
+    dt["capBssid"] = "b0:d5:9d:45:b9:85"
+    dt["bssid"] = bssid
+    dt["mapSP"] = "t"
+    dt["userToken"] = ""
+    dt["verName"] = "4.1.8"
+    dt["ssid"] = ssid
+    dt["verCode"] = "3028"
+    dt["uhid"] = "a0000000000000000000000000000001"
+    dt["lati"] = "30.579577"
+    dt["dhid"] = "9374df1b6a3c4072a0271d52cbb2c7b6"
+    dt = json.dumps(dt, ensure_ascii=False, separators=(",", ":"))
+
+    dt = quote(dt)
+    j = len(dt)
+    i = 0
+    while i < 16 - j % 16:
+        dt = dt + " "
+        i = i + 1
+    cipher = AES.new(b"!I50#LSSciCx&q6E", AES.MODE_CBC, b"$t%s%12#2b474pXF")
+    ed = cipher.encrypt(dt.encode()).hex().upper()
+    data = {}
+    data["appId"] = "A0008"
+    data["pid"] = "00300109"
+    data["ed"] = ed
+    data["st"] = "m"
+    data["et"] = "a"
+    ss = ""
+    for key in sorted(data):
+        ss = ss + data[key]
+    salt = "*Lm%qiOHVEedH3%A^uFFsZvFH9T8QAZe"
+    sign = getMd5(ss + salt)
+    data["sign"] = sign
+    url = "http://ap.51y5.net/ap/fa.sec"
+    post_data = urlencode(data)
+    req = urllib2.urlopen(url, post_data.encode())
+    content = req.read()
+    result = json.loads(content.decode("utf-8"))
+    if len(result["aps"]) == 0:
+        print("Not Found")
+        sys.exit()
+    epwd = result["aps"][0]["pwd"]
+    cipher = AES.new(b"!I50#LSSciCx&q6E", AES.MODE_CBC, b"$t%s%12#2b474pXF")
+    pdd = cipher.decrypt(epwd.decode("hex"))
+    length = int(pdd[:3])
+    pwd = pdd[3:][:length]
+    print("password is: " + pwd)
+
+
 r0 = openwrtWIFIList()
-r1 = openwrtWIFIList("radio1")
+# r1 = openwrtWIFIList("radio1")
 
-
-dt = collections.OrderedDict()
-dt["origChanId"] = "xiaomi"
-dt["appId"] = "A0008"
-dt["ts"] = "1459936625905"
-dt["netModel"] = "w"
-dt["chanId"] = "guanwang"
-dt["imei"] = "357541051318147"
-dt["qid"] = ""
-dt["mac"] = "e8:92:a4:9b:16:42"
-dt["capSsid"] = "hijack"
-dt["lang"] = "cn"
-dt["longi"] = "103.985752"
-dt["nbaps"] = ""
-dt["capBssid"] = "b0:d5:9d:45:b9:85"
-dt["bssid"] = "b0:d5:9d:56:82:10"
-dt["mapSP"] = "t"
-dt["userToken"] = ""
-dt["verName"] = "4.1.8"
-dt["ssid"] = "360免费WiFi-10"
-dt["verCode"] = "3028"
-dt["uhid"] = "a0000000000000000000000000000001"
-dt["lati"] = "30.579577"
-dt["dhid"] = "9374df1b6a3c4072a0271d52cbb2c7b6"
-dt = json.dumps(dt, ensure_ascii=False, separators=(",", ":"))
-dt = urllib.quote(dt)
-j = len(dt)
-i = 0
-while i < 16 - j % 16:
-    dt = dt + " "
-    i = i + 1
-cipher = AES.new(b"!I50#LSSciCx&q6E", AES.MODE_CBC, b"$t%s%12#2b474pXF")
-ed = cipher.encrypt(dt).encode("hex").upper()
-data = {}
-data["appId"] = "A0008"
-data["pid"] = "00300109"
-data["ed"] = ed
-data["st"] = "m"
-data["et"] = "a"
-ss = ""
-for key in sorted(data):
-    ss = ss + data[key]
-salt = "*Lm%qiOHVEedH3%A^uFFsZvFH9T8QAZe"
-sign = getMd5(ss + salt)
-data["sign"] = sign
-url = "http://ap.51y5.net/ap/fa.sec"
-post_data = urllib.urlencode(data)
-req = urllib2.urlopen(url, post_data)
-content = req.read()
-result = json.loads(content.decode("utf-8"))
-if len(result["aps"]) == 0:
-    print("Not Found")
-    sys.exit()
-epwd = result["aps"][0]["pwd"]
-cipher = AES.new(b"!I50#LSSciCx&q6E", AES.MODE_CBC, b"$t%s%12#2b474pXF")
-pdd = cipher.decrypt(epwd.decode("hex"))
-length = int(pdd[:3])
-pwd = pdd[3:][:length]
-print("password is: " + pwd)
+for i in r0["result"][1]["results"]:
+    print(i)
+    if "ssid" not in i:
+        continue
+    getWIFI(i["ssid"], i["bssid"])
