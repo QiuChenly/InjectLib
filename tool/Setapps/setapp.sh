18b1ef518cb10607a77cca01b8408ba8a53cfb15
QiuChenly <QiuChenly@outlook.com>
2024-03-08 00:36:25 +0800

    fix: setapp inject error in python and add more apps.

--- tool/Setapps/setapp.sh
+++ tool/Setapps/setapp.sh
@@ -1,2 +1,2 @@
 chmod +x tool/optool
-tool/optool install -p /Applications/Setapp.app/Contents/Frameworks/libInjectLib.dylib -t /Applications/Setapp.app/Contents/Library/LaunchServices/Setapp.app/Contents/MacOS/SetappAgent  --resign
+tool/optool install -p /Applications/Setapp.app/Contents/Frameworks/libInjectLib.dylib -t /Applications/Setapp.app/Contents/Library/LaunchServices/Setapp.app/Contents/MacOS/SetappAgent --resign
