d4d0d98f0db7645d21b43bd92193ed0416364308
QiuChenly <QiuChenly@outlook.com>
2024-04-06 16:14:53 +0800

    fix stash codesign.

--- tool/stash_x.sh
+++ tool/stash_x.sh
@@ -1,7 +1,7 @@
 echo "是否全新安装Stash?"
 echo "这将删除你的默认配置信息.请先备份配置信息到其他位置."
 read -p "(y/n,默认n):" option
-if [ $option = 'y' ];then             #判断用户是否输入，如果未输入则打印error
+if [ $option = 'y' ]; then #判断用户是否输入，如果未输入则打印error
   sudo /bin/launchctl unload /Library/LaunchDaemons/ws.stash.app.mac.daemon.helper.plist
   sudo /bin/rm /Library/LaunchDaemons/ws.stash.app.mac.daemon.helper.plist
   sudo /bin/rm /Library/PrivilegedHelperTools/ws.stash.app.mac.daemon.helper
