0e634233c43f17afea3c3d5dbfeab5533ca6d465
X1a0He <x1a0he@outlook.com>
2024-08-02 13:34:37 +0800

    Update: iStat Menus 7 (2189)

---
+++ tool/iStatMenus.sh
@@ -0,0 +1,6 @@
+sudo tool/insert_dylib '/Applications/iStat Menus.app/Contents/Frameworks/91QiuChenly.dylib' "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/MacOS/Components_backup" "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/MacOS/Components"
+/usr/libexec/PlistBuddy -c "Set :SMPrivilegedExecutables:com.bjango.istatmenus.installer 'identifier \\\"com.bjango.istatmenus.installer\\\"'" "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/Resources/iStat Menus Helper.app/Contents/Info.plist"
+/usr/bin/codesign -f -s - --timestamp=none --all-architectures --deep "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/Resources/com.bjango.istatmenus.daemon"
+/usr/bin/codesign -f -s - --timestamp=none --all-architectures --deep "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/Resources/iStat Menus Helper.app"
+/usr/bin/codesign -f -s - --timestamp=none --all-architectures --deep "/Applications/iStat Menus.app/Contents/Resources/Components.bundle"
+xattr -cr "/Applications/iStat Menus.app/Contents/Resources/Components.bundle"
