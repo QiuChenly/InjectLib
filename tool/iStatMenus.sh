ff131b18f2d008b37e61d7a63612c02f48a30e9a
X1a0He <x1a0he@outlook.com>
2024-08-03 13:55:07 +0800

    Update: fix iStat Menus 7

--- tool/iStatMenus.sh
+++ tool/iStatMenus.sh
@@ -1,3 +1,4 @@
+cp "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/MacOS/Components" "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/MacOS/Components_backup"
 sudo tool/insert_dylib '/Applications/iStat Menus.app/Contents/Frameworks/91QiuChenly.dylib' "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/MacOS/Components_backup" "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/MacOS/Components"
 /usr/libexec/PlistBuddy -c "Set :SMPrivilegedExecutables:com.bjango.istatmenus.installer 'identifier \\\"com.bjango.istatmenus.installer\\\"'" "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/Resources/iStat Menus Helper.app/Contents/Info.plist"
 /usr/bin/codesign -f -s - --timestamp=none --all-architectures --deep "/Applications/iStat Menus.app/Contents/Resources/Components.bundle/Contents/Resources/com.bjango.istatmenus.daemon"
