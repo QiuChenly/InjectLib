f2c438d041fc27e451363f3995756cacde923db1
QiuChenly <QiuChenly@outlook.com>
2024-08-10 20:05:55 +0800

    mv arm_mac_user64 for arm panic.

--- tool/copyLicense.sh
+++ tool/copyLicense.sh
@@ -2,3 +2,6 @@ cp tool/idalic.hexlic /Users/qiuchenly/.idapro/idalic.hexlic
 cp "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida64" /tmp/ida
 codesign -fs - /tmp/ida
 cp /tmp/ida "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida64"
+
+# 还没处理好 先删除吧
+mv "/Applications/IDA Professional 9.0.app/Contents/MacOS/plugins/arm_mac_user64.dylib" "/Applications/IDA Professional 9.0.app/Contents/MacOS/arm_mac_user64.backup"
