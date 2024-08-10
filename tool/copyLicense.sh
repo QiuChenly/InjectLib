c96bcbfa79c0b8ba446ceeab53724559ed7690ec
QiuChenly <QiuChenly@outlook.com>
2024-08-10 20:34:32 +0800

    fix bad ida cp license path

--- tool/copyLicense.sh
+++ tool/copyLicense.sh
@@ -1,4 +1,4 @@
-cp tool/idalic.hexlic /Users/qiuchenly/.idapro/idalic.hexlic
+cp tool/idalic.hexlic "/Applications/IDA Professional 9.0.app/Contents/MacOS/idalic.hexlic"
 cp "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida64" /tmp/ida
 codesign -fs - /tmp/ida
 cp /tmp/ida "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida64"
