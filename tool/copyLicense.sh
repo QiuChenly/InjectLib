13fc2deec5ee5562efb7f6a564f97b4408747caa
秋城落叶 <qiuchenly@outlook.com>
2025-01-15 17:41:01 +0800

    feat(IDA): 支持Mac ARM/Intel的 IDA SP1 (9.0.241217)泄漏版破解

--- tool/copyLicense.sh
+++ tool/copyLicense.sh
@@ -1,9 +1,4 @@
-cp tool/idalic.hexlic "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida.hexlic"
-cp "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida64" /tmp/ida
+cp tool/CoreInject.dylib '/Applications/IDA Professional 9.0.app/Contents/MacOS/91QiuChenly.dylib'
+cp "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida" /tmp/ida
 codesign -fs - /tmp/ida
-cp /tmp/ida "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida64"
-
-if [ -f "/Applications/IDA Professional 9.0.app/Contents/MacOS/plugins/arm_mac_user64.dylib" ]; then
-    # 还没处理好 先删除吧
-    mv "/Applications/IDA Professional 9.0.app/Contents/MacOS/plugins/arm_mac_user64.dylib" "/Applications/IDA Professional 9.0.app/Contents/MacOS/arm_mac_user64.backup"
-fi
+cp /tmp/ida "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida"
