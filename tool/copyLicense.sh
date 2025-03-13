c1c8266fc8c21eadb5d27abaf9ae68071c5832dd
秋城落叶 <qiuchenly@outlook.com>
2025-03-13 14:06:15 +0800

    fix wrong path

--- tool/copyLicense.sh
+++ tool/copyLicense.sh
@@ -1,4 +1,4 @@
-cp tool/CoreInject.dylib '/Applications/IDA Professional 9.0.app/Contents/MacOS/91QiuChenly.dylib'
-cp "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida" /tmp/ida
+cp tool/CoreInject.dylib '/Applications/IDA Professional 9.1.app/Contents/MacOS/91QiuChenly.dylib'
+cp "/Applications/IDA Professional 9.1.app/Contents/MacOS/ida" /tmp/ida
 codesign -fs - /tmp/ida
-cp /tmp/ida "/Applications/IDA Professional 9.0.app/Contents/MacOS/ida"
+cp /tmp/ida "/Applications/IDA Professional 9.1.app/Contents/MacOS/ida"
