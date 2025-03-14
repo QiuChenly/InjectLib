788cdb821ad7a2fd1d59e70c1d03e3cccdcdc00b
QiuChenly <QiuChenly@outlook.com>
2025-03-15 01:24:32 +0800

    1. 支持navicat premium mas版本17.2.2. 2. 修复IDA9.1在arm64机器上激活时因为没有签名dylib导致arm机器打不开报错的问题。

--- tool/copyLicense.sh
+++ tool/copyLicense.sh
@@ -1,3 +1,4 @@
+codesign -fs - tool/CoreInject.dylib
 cp tool/CoreInject.dylib '/Applications/IDA Professional 9.1.app/Contents/MacOS/91QiuChenly.dylib'
 cp "/Applications/IDA Professional 9.1.app/Contents/MacOS/ida" /tmp/ida
 codesign -fs - /tmp/ida
