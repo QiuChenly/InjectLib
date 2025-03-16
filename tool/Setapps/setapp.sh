a9d9a2ced7319e5b89f26d1cea47ead8c0a2c933
QiuChenly <QiuChenly@outlook.com>
2025-03-17 02:25:11 +0800

    Fix Setapp-99。 - 修复对99版本的setapp破解支持。 - 测试了cleanmymac版本和TablePlus版本的setapp破解

--- tool/Setapps/setapp.sh
+++ tool/Setapps/setapp.sh
@@ -1,3 +1,3 @@
 cp /Applications/Setapp.app/Contents/Library/LaunchServices/Setapp.app/Contents/MacOS/SetappAgent /Applications/Setapp.app/Contents/Library/LaunchServices/Setapp.app/Contents/MacOS/SetappAgent_backup
-tool/insert_dylib /Applications/Setapp.app/Contents/Frameworks/91QiuChenly.dylib /Applications/Setapp.app/Contents/Library/LaunchServices/Setapp.app/Contents/MacOS/SetappAgent_backup /Applications/Setapp.app/Contents/Library/LaunchServices/Setapp.app/Contents/MacOS/SetappAgent
+tool/insert_dylib /Applications/Setapp.app/Contents/Frameworks/CoreInject.dylib /Applications/Setapp.app/Contents/Library/LaunchServices/Setapp.app/Contents/MacOS/SetappAgent_backup /Applications/Setapp.app/Contents/Library/LaunchServices/Setapp.app/Contents/MacOS/SetappAgent
 codesign -fs - /Applications/Setapp.app/Contents/Library/LaunchServices/Setapp.app
